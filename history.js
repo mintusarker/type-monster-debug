9dd18bcac18ead6e6f3f8e4315eb994881771e49
mintusarker <sarkermintu707@gmail.com>
2022-09-15 01:46:28 +0600

    column responsive for device

--- history.js
+++ history.js
@@ -24,16 +24,16 @@ function addHistory(questionText, timeTaken, errorCount) {
 function displayHistory() {
   histories.innerHTML = "";
   const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
-
+  
   previousTests.forEach((test) => {
     const newRow = document.createElement("div");
     newRow.classList.add("card");
-
+    
     newRow.innerHTML = `
-  <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+    <h3>${test.questionText}</h3>
+    <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
-  `;
+    `;
 
     histories.appendChild(newRow);
   });
